65b4ddf12becd4de277fe3348448bffea94e3f99
srgx2013 <srgx2012@gmail.com>
2025-02-24 19:58:33 -0600

    flujo

---
+++ Javascript/control_de_flujo/02-else.js
@@ -0,0 +1,9 @@
+let edad = 10;
+
+if (edad > 17) {
+  console.log("Eres mayor de edad");
+} else if (edad > 13) {
+  console.log("Usuario necesita estar acompanado de sus padres");
+} else {
+  console.log("no puede ingresar");
+}
